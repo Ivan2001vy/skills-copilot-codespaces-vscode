a9c8bebaca6ae3c6ea49480302e77430542c7b7b
Ivan2001vy <145730901+Ivan2001vy@users.noreply.github.com>
2023-11-27 22:47:44 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/components/members/member/skills/skills.html',
+    controller: 'SkillsMemberController',
+    controllerAs: 'skillsMemberCtrl'
+  };
+}
